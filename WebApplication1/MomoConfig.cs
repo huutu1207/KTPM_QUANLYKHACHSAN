f6b00ba020bec54bd9d088db45f7617edfa3ef97
Htu <lehuutu1207@gmail.com>
2025-05-19 12:00:52 +0700

    can use atm of momo and vnpay

--- WebApplication1/MomoConfig.cs
+++ WebApplication1/MomoConfig.cs
@@ -7,10 +7,13 @@ namespace WebApplication1
 {
     public class MomoConfig
     {
+
         public static string PartnerCode = "MOMO";
         public static string AccessKey = "F8BBA842ECF85";
         public static string SecretKey = "K951B6PE1waDMi640xX08PD3vg6EkVlz";
         public static string Endpoint = "https://test-payment.momo.vn/v2/gateway/api/create";
+        public static string apiUrl = "https://test-payment.momo.vn/gw_payment/transactionProcessor";
+
         public static string ReturnUrl = "https://localhost:44354/DatPhong/ReturnUrl";
         public static string NotifyUrl = "https://webhook.site/454e7b77-f177-4ece-8236-ddf1c26ba7f8"; // URL callback test
     }
